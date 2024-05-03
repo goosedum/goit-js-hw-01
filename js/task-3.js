112af441834dd80e5851da03c8f7048ec92a9bab
goosedum <154931486+goosedum@users.noreply.github.com>
2024-05-03 19:29:51 +0300

    ь
    л

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,18 @@
-function checkForSpam(message) {
-  const newMessage = message.toLowerCase();
-  if (newMessage.includes('spam') || newMessage.includes('sale')) {
-    return true;
-  } else return false;
+function filterArray(numbers, value) {
+    // створення порожнього масиву
+    const newNumbers = [];
+    // використання циклу для ітерації кожного елемента
+    for (let i = 0; numbers.length > i; i++) {
+    // використання умовного оператора if для перевірки кожного елемента і додавання їх до масиву
+        if (numbers[i] > value) {
+            newNumbers.push(numbers[i]);
+        }
+    }
+     return newNumbers;
 }
 
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
