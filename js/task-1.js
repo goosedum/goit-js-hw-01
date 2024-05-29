a590f3a3ace488b7050a2dab83b9d69c60756ba4
goosedum <154931486+goosedum@users.noreply.github.com>
2024-05-29 21:52:59 +0300

    nnn

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,24 @@
-function slugify(title) {
-    // Перетворюємо рядок у нижній регістр
-    const lowerCaseTitle = title.toLowerCase();
-    
-    // Розділяємо рядок на масив слів
-    const words = lowerCaseTitle.split(' ');
-    
-    // Об'єднуємо слова через тире
-    const slug = words.join('-');
-    
-    // Повертаємо
-    return slug;
-}
+function isEnoughCapacity(products, containerSize) {
+  const product = { products }
+  let sum = 0
+  for (const product of Object.values(products)) {
+    sum += product;}
+  if (sum<= containerSize) {return true}
+  else {return false}
+  }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); 
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); 
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+);
