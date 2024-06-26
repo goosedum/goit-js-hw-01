3faad86255bb4404d3cb0326b1097f0039d2a24e
goosedum <154931486+goosedum@users.noreply.github.com>
2024-06-26 22:09:54 +0300

    3

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,11 +1,11 @@
-// Описаний в документації
+
 import flatpickr from 'flatpickr';
-// Додатковий імпорт стилів
+
 import 'flatpickr/dist/flatpickr.min.css';
 
-// Описаний у документації
+
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
+
 import 'izitoast/dist/css/iziToast.min.css';
 
 const refs = {
